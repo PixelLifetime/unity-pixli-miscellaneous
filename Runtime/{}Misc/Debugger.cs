02a3ae393b64b57c34f56d0ea869f5b5c0afd129
Pixel Lifetime <maxymkarpinsky@gmail.com>
2021-06-21 01:20:48 +0300

    Refactoring update.

--- Runtime/{}Misc/Debugger.cs
+++ Runtime/{}Misc/Debugger.cs
@@ -1,13 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
-using UDebug = UnityEngine.Debug;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-namespace GMTKGameJam2020
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+using HDebug = PixLi.Debug;
+
+namespace PixLi
 {
 	public class Debugger : MonoBehaviour
 	{
-		public void Debug(string message) => UDebug.Log(message: message);
+		public void Debug(string message) => HDebug.Log(message);
+
+#if UNITY_EDITOR
+		//protected virtual void OnDrawGizmos()
+		//{
+		//}
+#endif
 	}
+
+#if UNITY_EDITOR
+	[CustomEditor(typeof(Debugger))]
+	[CanEditMultipleObjects]
+	public class DebuggerEditor : MultiSupportEditor
+	{
+#pragma warning disable 0219, 414
+		private Debugger _sDebugger;
+#pragma warning restore 0219, 414
+
+		public override void OnEnable()
+		{
+			base.OnEnable();
+
+			this._sDebugger = this.target as Debugger;
+		}
+
+		public override void MainDrawGUI()
+		{
+			this.DrawDefaultInspector();
+		}
+	}
+#endif
 }
