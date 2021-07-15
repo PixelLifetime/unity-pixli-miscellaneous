9cc0653db25f3618cbdec7d6562d00b0c093081b
Pixel Lifetime <maxymkarpinsky@gmail.com>
2021-07-15 18:55:54 +0300

    GJL game jam changes.

--- Runtime/{}Provider/{}Examples/TestSelectorAdapter.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "[Test] Selector Adapter", menuName = "[TESTs]/[Test] Selector Adapter")]
-public class TestSelectorAdapter : SelectorAdapter<TestProvider.TestSelector, TestProvider, string, int>
-{
-}
